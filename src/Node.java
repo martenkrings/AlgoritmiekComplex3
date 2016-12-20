442493f7a1075f6095e88b4bcf05773d80c96ba5
Marten <krings.marten@gmail.com>
2016-12-20 14:24:08 +0100

    getEdges()

--- src/Node.java
+++ src/Node.java
@@ -1,10 +1,27 @@
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class Node {
     private nodeColor color;
-    private HashMap<nodeColor, Node> paths;
+    private HashMap<Node, nodeColor> edges;
+
+    public nodeColor getColor() {
+        return color;
+    }
+
+    public HashSet<Node> getEdges(nodeColor c) {
+        HashSet<Node> nodes = new HashSet<>();
+
+        for (Node n: edges.keySet()) {
+            if (edges.get(n) == c) {
+                nodes.add(n);
+            }
+        }
+
+        return nodes;
+    }
 
     private enum nodeColor {
-        BLACK, GREEN, ORANGE, PINK
+        BLACK, GREEN, ORANGE, PINK, FINISH
     }
 }
